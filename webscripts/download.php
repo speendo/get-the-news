3dca187861de36d52e82a9d2277b2159005d8e03
Marcel Jira <marcel.jira@gmail.com>
2012-05-13 18:29:00 +0200

    outsourced download-script to functions.php

--- webscripts/download.php
+++ webscripts/download.php
@@ -19,51 +19,13 @@
 ### You should have received a copy of the GNU General Public License
 ### along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
-
+include("functions.php");
 
 include("settings.php");
 
 $path = decrypt($_GET['path'], $encryptionKey);
 $mimeType = $_GET['mimeType'];
 
-if(!file_exists($path)) {
-	// File doesn't exist, output error
-	die('file not found');
-} else {
-	$size = filesize($path);
-	$file = basename($path);
+startDownload($path);
 
-	// Set headers
-	header("Pragma: public"); // required
-	header("Expires: 0");
-	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-	header("Cache-Control: private",false); // required for certain browsers 
-	header("Content-Description: File Transfer");
-	header("Content-Disposition: attachment; filename=\"$file\"");
-	header("Content-Type: $mimeType");
-	header("Content-Transfer-Encoding: binary");
-	header("Content-Length: $size");
-	// Read the file from disk
-	readfile($path);
-}
-
-exit();
-
-function decrypt($string, $encryptionKey) {
-	if ($encryptionKey == "") {
-		return $string;
-	} else {
-		$result = '';
-		$string = base64_decode($string);
-
-		for($i=0; $i<strlen($string); $i++) {
-			$char = substr($string, $i, 1);
-			$keychar = substr($encryptionKey, ($i % strlen($encryptionKey))-1, 1);
-			$char = chr(ord($char)-ord($keychar));
-			$result.=$char;
-		}
-
-	return $result;
-	}
-}
 ?>
--- webscripts/functions.php
+++ webscripts/functions.php
@@ -50,4 +50,47 @@ function encrypt($string, $encryptionKey) {
 		return base64_encode($result);
 	}
 }
+
+function decrypt($string, $encryptionKey) {
+	if ($encryptionKey == "") {
+		return $string;
+	} else {
+		$result = '';
+		$string = base64_decode($string);
+
+		for($i=0; $i<strlen($string); $i++) {
+			$char = substr($string, $i, 1);
+			$keychar = substr($encryptionKey, ($i % strlen($encryptionKey))-1, 1);
+			$char = chr(ord($char)-ord($keychar));
+			$result.=$char;
+		}
+
+	return $result;
+	}
+}
+
+function startDownload($path) {
+	if(!file_exists($path)) {
+		// File doesn't exist, output error
+		exit('file not found');
+	} else {
+		$size = filesize($path);
+		$file = basename($path);
+
+		// Set headers
+		header("Pragma: public"); // required
+		header("Expires: 0");
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Cache-Control: private",false); // required for certain browsers 
+		header("Content-Description: File Transfer");
+		header("Content-Disposition: attachment; filename=\"$file\"");
+		header("Content-Type: $mimeType");
+		header("Content-Transfer-Encoding: binary");
+		header("Content-Length: $size");
+		// Read the file from disk
+		readfile($path);
+	}
+
+	exit();
+}
 ?>
